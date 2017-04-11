0010fdfc321015d32d40291f610d6a228ec740ae
Rashid Ali [Associate Technical Consultant] <r.ali@inboxbiz.com>
2017-04-11 12:56:21 +0500

    first commit

---
+++ rpackage/R/Hello.R
@@ -0,0 +1,14 @@
+
+# print Hello 
+#
+# This functions prints Hello 
+# @param fname 
+# @param last-name
+# @examples 
+# hello (fname="Rashid",last_name ="Ali")
+
+hello <- function (fname , last_name ) {
+  cat (paste("Hello ",fname,last_name))
+  
+  
+}
